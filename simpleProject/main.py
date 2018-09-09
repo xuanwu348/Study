a3e364706e664763ac349cd7d0d11f58ead4f1be
xuanwu348 <xuanwu348@163.com>
2018-09-09 08:39:43 +0000

    add comment tag db table

--- simpleProject/main.py
+++ simpleProject/main.py
@@ -26,13 +26,29 @@ class User(db.Model):
     def __repr__(self):
         return "<User '{}'>".format(self.username)
 
+tags = db.Table('post_tags',
+            db.Column("post_id", db.Integer, db.ForeignKey("post.id")),
+            db.Column("tag_id", db.Integer, db.ForeignKey("tag.id"))
+        )
+
+
 
 class Post(db.Model):
     id = db.Column(db.Integer(), primary_key = True)
     title = db.Column(db.String(255))
     text = db.Column(db.Text())
-    publish_data = db.Column(db.DateTime())
+    publish_date = db.Column(db.DateTime())
+    comments = db.relationship(
+                  "Comment",
+                  backref='Post',
+                  lazy = 'dynamic'
+               )
     user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
+    tags = db.relationship(
+               'Tag',
+               secondary = tags,
+               backref = db.backref('posts',lazy="dynamic")
+            )
 
     def __init__(self, title):
         self.title = title
@@ -40,7 +56,27 @@ class Post(db.Model):
     def __repr__(self):
         return "<Post '{}'>".format(self.title)
 
+class Tag(db.Model):
+    id = db.Column(db.Integer(), primary_key = True)
+    title = db.Column(db.String(255))
 
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Tag '{}'>".format(self.title)
+
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime())
+    post_id = db.Column(db.Integer(), db.ForeignKey("post.id"))
+
+    def __repr__(self):
+        return "<comment '{}'>".format(self.text[:15])
 
 if __name__ == "__main__":
     app.run()
