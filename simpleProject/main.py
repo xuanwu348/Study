bd5421693da224a7495798eee43d3e7c6a57562d
xuanwu348 <xuanwu348@163.com>
2018-09-09 03:28:22 +0000

    add Post table

--- simpleProject/main.py
+++ simpleProject/main.py
@@ -14,12 +14,31 @@ class User(db.Model):
     id = db.Column(db.Integer(), primary_key = True)
     username = db.Column(db.String(255))
     passwd = db.Column(db.String(255))
+    posts = db.relationship(
+               'Post',
+               backref = 'user',
+               lazy = 'dynamic'
+            )
 
     def __init__(self, username):
         self.username = username
 
     def __repr__(self):
         return "<User '{}'>".format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key = True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_data = db.Column(db.DateTime())
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Post '{}'>".format(self.title)
 
 
 
