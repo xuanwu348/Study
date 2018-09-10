88a7fec7d51e0aad36f2f26eeceb58f2cf2c6a06
xuanwu348 <xuanwu348@163.com>
2018-09-10 14:50:23 +0000

    add more code to home.html

--- simpleProject/add_data.py
+++ simpleProject/add_data.py
@@ -12,7 +12,7 @@ for i in range(100):
     new_post = Post("Post " + str(i))
     new_post.user = user
     new_post.publish_date = datetime.datetime.now()
-    new_text = s
+    new_post.text = s
     new_post.tags = random.sample(taglist, random.randint(1,3))
     db.session.add(new_post)
 
