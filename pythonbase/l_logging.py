b0c49f7bfea7b09ef43b9302454d5b88a2056829
xuanwu348 <xuanwu348@163.com>
2018-09-19 23:30:39 +0800

    update

---
+++ pythonbase/l_logging.py
@@ -0,0 +1,13 @@
+import logging
+from multiprocessing import Pool
+from os import getpid
+
+def runs_in_subprocess():
+    logging.info("I am in the child, with PID {}".format(getpid()))
+
+if __name__ == "__main__":
+    logging.basicConfig(format="sssss %(message)s", level=logging.DEBUG)
+    logging.info("I am in the parent, with PID {}".format(getpid()))
+    pool = Pool(processes = 4)
+    pool.apply(runs_in_subprocess)
+    pool.terminate()
