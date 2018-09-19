381691d322b574894d1d640bd87be8db4a6006d6
xuanwu348 <xuanwu348@163.com>
2018-09-19 23:33:12 +0800

    update l_thr.py

--- pythonbase/l_logging.py
+++ pythonbase/l_logging.py
@@ -8,6 +8,12 @@ def runs_in_subprocess():
 if __name__ == "__main__":
     logging.basicConfig(format="sssss %(message)s", level=logging.DEBUG)
     logging.info("I am in the parent, with PID {}".format(getpid()))
+    """
+    with version > 3.2 can use as below
+    with Pool(processes = 4) as pool:
+        pool.apply(.......)
+    otherwise prompt AttributeError: no __exit__
+    """
     pool = Pool(processes = 4)
     pool.apply(runs_in_subprocess)
     pool.terminate()
