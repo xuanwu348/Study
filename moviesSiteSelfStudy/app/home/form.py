0b36a95df885ea76e8384c6d4d388ad626e40269
xuanwu348 <xuanwu348@163.com>
2018-09-28 17:47:49 +0000

    add userlogin and logout function

--- moviesSiteSelfStudy/app/home/form.py
+++ moviesSiteSelfStudy/app/home/form.py
@@ -89,5 +89,36 @@ class RegistForm(FlaskForm):
         if user != 0:
             raise ValidationError("手机号码已被注册！")
 
+class LoginForm(FlaskForm):
+    account = StringField(
+            label = "账号",
+            validators = [
+                DataRequired("请输入账号")
+                ],
+            description="账号",
+            render_kw = {
+                "id":"input_contact",
+                "class":"form-control input-lg", 
+                "placeholder":"用户名/邮箱/手机号码"
+                }
+            )
+    pwd = PasswordField(
+            label = "密码",
+            validators = [
+                DataRequired("请输入密码！")
+                ],
+            description = "密码",
+            render_kw = {
+                "id":"input_password",
+                "class":"form-control input-lg" ,
+                "placeholder":"密码"
+                }
+            )
+    submit = SubmitField(
+            "登录",
+            render_kw = {
+                   "class":"btn btn-lg btn-success btn-block",
+                }
+            )
 
 
--- moviesSiteSelfStudy/app/home/views.py
+++ moviesSiteSelfStudy/app/home/views.py
@@ -1,22 +1,53 @@
 #_*_ encoding:utf-8 _*_
 from . import home
-from flask import render_template, redirect, url_for, session, flash
-from app.home.form import RegistForm
+from flask import render_template, redirect, url_for, session, flash, request
+from app.home.form import RegistForm, LoginForm
 import uuid
-from app.models import User
+from app.models import User,Userlog
 from werkzeug.security import generate_password_hash
 from app import db
+from functools import wraps
+
+def user_login_req(f):
+    @wraps(f)
+    def decorate_function(*args, **kwargs):
+        if "user" not in session:
+            return redirect(url_for("home.login", next=request.url))
+        return f(*args,**kwargs)
+    return decorate_function
 
 @home.route("/")
 def index():
     return render_template("home/index.html")
 
-@home.route("/login/")
+@home.route("/login/", methods=["GET","POST"])
 def login():
-    return render_template("home/login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        data = form.data
+        user = User.query.filter_by(name=data["account"]).first()
+        if not user:
+            flash("登录失败，账号或者密码错！","err")
+            return redirect(url_for("home.login"))
+        if not user.check_pwd(data["pwd"]):
+            flash("登录失败，账号或者密码错！","err")
+            return redirect(url_for("home.login"))
+        session["user"] = data["account"]
+        session["user_id"] = user.id
+        flash("登录成功", "OK")
+        userlog = Userlog(
+                user_id = user.id,
+                ip = request.remote_addr,
+                )
+        db.session.add(userlog)
+        db.session.commit()
+        return redirect(url_for("home.user"))
+    return render_template("home/login.html", form = form)
 
 @home.route("/logout/")
 def logout():
+    session.pop("user",None)
+    session.pop("user_id", None)
     return redirect(url_for("home.login"))
 
 @home.route("/regist/", methods=["GET","POST"])
@@ -33,26 +64,27 @@ def regist():
                 )
         db.session.add(user)
         db.session.commit()
-        flash("注册成功！", "OK")
-        session['user'] = data["name"]
-        user_id = User.query.filter_by(name=data['name']).first()
-        session['user_id'] = user_id.id
-        return redirect(url_for('home.user'))
+        flash("注册成功！请登录......", "OK")
+        return redirect(url_for('home.login'))
     return render_template("home/regist.html", form = form)
 
 @home.route("/user/")
+@user_login_req
 def user():
     return render_template("home/user.html")
 
 @home.route("/pwd/")
+@user_login_req
 def pwd():
     return render_template("home/pwd.html")
 
 @home.route("/loginlog/")
+@user_login_req
 def loginlog():
     return render_template("home/loginlog.html")
 
 @home.route("/comments/")
+@user_login_req
 def comments():
     return render_template("home/comments.html")
 
