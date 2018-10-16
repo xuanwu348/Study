2ef24dcdac6bdac67698138722657db08e8c6937
xuanwu348 <xuanwu348@163.com>
2018-10-16 14:51:02 +0000

    workqueue done

--- pythonbase/rabbitMQ/WorkQueue/woker.py
+++ pythonbase/rabbitMQ/WorkQueue/woker.py
@@ -16,6 +16,8 @@ def callback(ch, method, properties, body):
 def myHandle(num, frame):
     exit()
 
+#n order to defeat that we can use the basic.qos method with the prefetch_count=1 setting. This tells RabbitMQ not to give more than one message to a worker at a time. Or, in other words, don't dispatch a new message to a worker until it has processed and acknowledged the previous one. Instead, it will dispatch it to the next worker that is not still busy.
+channel.basic_qos(prefetch_count=1)
 channel.basic_consume(callback, 
         queue='task_queue',
         #no_ack=True
