a74afddbaacba87413e691f1ea1fca6ddaea3b3c
xx <xx@intel.com>
2018-11-01 00:28:33 +0800

    add class Config to config

---
+++ fab_t/fabfile_config.py
@@ -0,0 +1,29 @@
+#encoding:utf-8
+from fabric import task, Config, Connection
+
+host = "10.240.109.121"
+config = Config()
+conf_dict = {
+            "user":"hddl",
+            "port":22,
+            "connect_kwargs":{"password":"intel123"},
+            "timeouts":{"connect":5},
+            "sudo":{'user':None, "prompt":"\\[sudo\\] password for hddl:", "password":"intel123"}
+         } 
+config.update(conf_dict)
+
+def update_config(connection):
+    run_hots = connection.host
+    c = Connection(run_hots, config=config)
+    return c
+    
+@task
+def getplatforminfo(c):
+    with update_config(c) as c:
+        c.run("uname -a")
+    
+
+if __name__ == "__main__":
+    c = Connection(host="10.240.109.121",config=config)
+    c.run("uname -a")
+
