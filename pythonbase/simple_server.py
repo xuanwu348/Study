e8eb1b3d199ebd084612764a08ca0c34cc6c4268
xuanwu348 <xuanwu348@163.com>
2019-03-03 08:24:41 +0000

    add player

--- pythonbase/simple_server.py
+++ pythonbase/simple_server.py
@@ -1,10 +1,14 @@
 #encoding:utf-8
-from flask import Flask, send_from_directory, abort
+from flask import Flask, send_from_directory, abort, render_template
 import os
 import random
 import sys
 
 app = Flask(__name__)
+
+
+if sys.version_info.major !=3:
+    raise Exception("Please use python with version greater or equal 3.4")
 
 namelists = {}
 for dirname, temp, filenames in os.walk("./"):
@@ -20,17 +24,20 @@ for dirname, temp, filenames in os.walk("./"):
 def index():
     content = ""
     for name in namelists.keys():
-        content += "<a href=./{0}>{0}</a></br>".format(name)
+        content += '<a href="{0}">{0}</a></br>'.format(name)
     return content
 
-@app.route("/<string:name>", methods=["GET"])
+@app.route("/<name>", methods=["GET"])
 def sendFile(name):
     try:
         if os.path.isfile(namelists[name]):
+            if name.endswith(".mp4"):
+                return render_template("player.html",filep=namelists[name])
             return send_from_directory("./", namelists[name], as_attachment=True)
-    except KeyError:
+    except KeyError as e:
+        print("\033[32m{} KeyError\033[0m".format(name.encode("utf-8")))
         pass
     abort(404)
     
 if __name__ == "__main__":
-    app.run(("0.0.0.0"),debug = False)
+    app.run(("0.0.0.0"),debug = True)
